2eb9aa21c5a4ed8d6373dfec3a95d04273e42c58
hminn <hmkim0008@gmail.com>
2020-04-24 19:35:44 +0900

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    """ Custom User Admin """
+
+    pass
