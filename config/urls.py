9c8530c8d5f7903c2924d45a4260d76ea5629b2e
hminn <hmkim0008@gmail.com>
2020-04-30 18:34:32 +0900

    Update urls.py
    - Add todos

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls", namespace="core")),
+    path("todos/", include("todos.urls", namespace="todos")),
 ]
