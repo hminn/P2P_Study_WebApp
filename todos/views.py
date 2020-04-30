6c793627ddbcc625e04f1e3e0fb73af6f2e6acb7
hminn <hmkim0008@gmail.com>
2020-04-30 18:36:41 +0900

    Update views.py
    - TodoList를 뿌려주기 위한 View

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from datetime import date
+from . import models
+from django.views.generic import ListView, TemplateView
 
-# Create your views here.
+
+class TodoListView(ListView):
+    model = models.TodoList
+    template_name = "todos.html"
+    context_object_name = "todos"
+
+
+class TodoListingView(TemplateView):
+    def get(self, request):
+        template_name = "todos.html"
+        todo_list = models.TodoList.objects.all()
+        filtered_list = todo_list.filter(user=request.user, created_date=date.today())
+        return render(request, template_name, {"todo_list": filtered_list})
