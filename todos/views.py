818c77830e1e21f138656ac6fccf35f3d4efcb51
hminn <hmkim0008@gmail.com>
2020-05-03 10:32:17 +0900

    Update views.py
    - Create, Delete에 대한 POST 처리를 각각 진행

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
 from datetime import date
-from . import models
-from django.views.generic import ListView, TemplateView
+from . import models, forms
+from django.views.generic import ListView, TemplateView, FormView
 
 
 class TodoListView(ListView):
@@ -10,9 +12,36 @@ class TodoListView(ListView):
     context_object_name = "todos"
 
 
-class TodoListingView(TemplateView):
+class TodoListingView(FormView):
+    template_name = "todos.html"
+    form_class = forms.TodoForm
+    success_url = reverse_lazy("todos:base")
+
     def get(self, request):
-        template_name = "todos.html"
         todo_list = models.TodoList.objects.all()
         filtered_list = todo_list.filter(user=request.user, created_date=date.today())
-        return render(request, template_name, {"todo_list": filtered_list})
+        form = forms.TodoForm()
+        return render(request, "todos.html", {"todo_list": filtered_list, "form": form})
+
+    def post(self, request, *args, **kwargs):
+
+        # todo 추가 버튼에 대한 POST : todo 추가 작업 진행
+        if "add-todo" in request.POST:
+            form = self.get_form()
+            obj = form.save(commit=False)
+            obj.user = request.user
+            obj.save()
+            if form.is_valid():
+                return self.form_valid(form)
+            else:
+                return self.form_invalid(form)
+
+        # todo 삭제 버튼에 대한 POST : todo 삭제 작업 진행
+        # Front-end에서 POST 요청 시,
+        # 해당 버튼의 name이 request.POST의 key로,
+        # 해당 버튼의 value는 name key의 value로 넘어온다.
+        if "del-todo" in request.POST:
+            todo_id = request.POST.get("del-todo")
+            del_query = models.TodoList.objects.filter(id=todo_id)
+            del_query.delete()
+            return HttpResponseRedirect(self.get_success_url())
