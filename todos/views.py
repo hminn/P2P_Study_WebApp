75df9a0fb4405a39c8585f081617d129ded2f426
hminn <hmkim0008@gmail.com>
2020-05-03 18:23:44 +0900

    Update views.py
    - TimeTable 페이지에서 다음과 같은 기능들을 구현
    > Today's TodoList Showing & TodoList checked Data 저장
    > 각 Part에 대한 TimeTask Cell 추가 작업 (CRD 기능)

--- todos/views.py
+++ todos/views.py
@@ -6,16 +6,10 @@ from . import models, forms
 from django.views.generic import ListView, TemplateView, FormView
 
 
-class TodoListView(ListView):
-    model = models.TodoList
-    template_name = "todos.html"
-    context_object_name = "todos"
-
-
-class TodoListingView(FormView):
+class TodoListView(FormView):
     template_name = "todos.html"
     form_class = forms.TodoForm
-    success_url = reverse_lazy("todos:base")
+    success_url = reverse_lazy("todos:todos")
 
     def get(self, request):
         todo_list = models.TodoList.objects.all()
@@ -45,3 +39,76 @@ class TodoListingView(FormView):
             del_query = models.TodoList.objects.filter(id=todo_id)
             del_query.delete()
             return HttpResponseRedirect(self.get_success_url())
+
+
+class TimeTableView(FormView):
+    template_name = "timetable.html"
+    form_class = forms.TimeTaskForm
+    success_url = reverse_lazy("todos:timetable")
+
+    def get(self, request):
+        todo_list = models.TodoList.objects.all()
+        timetask_list = models.TimeTask.objects.all()
+        filtered_list = todo_list.filter(user=request.user, created_date=date.today())
+        filtered_one_list = timetask_list.filter(
+            user=request.user, created_date=date.today(), part="one"
+        )
+        filtered_two_list = timetask_list.filter(
+            user=request.user, created_date=date.today(), part="two"
+        )
+        filtered_three_list = timetask_list.filter(
+            user=request.user, created_date=date.today(), part="three"
+        )
+        form = forms.TodoForm()
+        return render(
+            request,
+            "timetable.html",
+            {
+                "todo_list": filtered_list,
+                "form": form,
+                "part_one": filtered_one_list,
+                "part_two": filtered_two_list,
+                "part_three": filtered_three_list,
+            },
+        )
+
+    def post(self, request, *args, **kwargs):
+        todo_list = models.TodoList.objects.all()
+        filtered_list = todo_list.filter(user=request.user, created_date=date.today())
+
+        if "add-task" in request.POST:
+            form = self.get_form()
+            obj = form.save(commit=False)
+            obj.user = request.user
+            obj.part = request.POST.get("add-task")
+            obj.save()
+            if form.is_valid():
+                return self.form_valid(form)
+            else:
+                return self.form_invalid(form)
+
+        if "del-task" in request.POST:
+            task_id = request.POST.get("del-task")
+            del_query = models.TimeTask.objects.filter(id=task_id)
+            del_query.delete()
+            return HttpResponseRedirect(self.get_success_url())
+
+        # Cheked Save 버튼에 대한 작업 : cheked data를 저장하는 작업 진행
+        # Save 버튼에 대한 POST인지 확인
+        # Checked List 여부를 확인
+        # 존재한다면 각각의 id에 맞는 TodoList의 Checked에 True 값 대입
+        # 존재하지 않는다면 모든 TodoList의 Checked에 False 값 대입
+
+        if "save" in request.POST:
+            checked_id = request.POST.getlist("check-todo")
+            if "check-todo" in request.POST:
+                for filtered_object in filtered_list:
+                    if str(filtered_object.id) in checked_id:
+                        filtered_object.checked = True
+                    else:
+                        filtered_object.checked = False
+                    filtered_object.save()
+            else:
+                filtered_list.update(checked=False)
+
+        return HttpResponseRedirect(self.get_success_url())
