a06bf13f4083dc3f9b7814af56149121b6c3039c
hminn <hmkim0008@gmail.com>
2020-05-03 18:20:46 +0900

    Update models.py
    - TimeTask Model 추가

--- todos/models.py
+++ todos/models.py
@@ -14,14 +14,38 @@ class TodoList(TimeStampedModel):
     created_date = models.DateField(auto_now_add=True)
 
 
+class TimeTask(TimeStampedModel):
+    """ TimeTask Model Definition """
+
+    PART_ONE = "one"
+    PART_TWO = "two"
+    PART_KAKAO = "three"
+
+    PART_CHOICES = (
+        (PART_ONE, "One"),
+        (PART_TWO, "Two"),
+        (PART_KAKAO, "Three"),
+    )
+    contents = models.TextField(null=True, blank=True)
+    user = models.ForeignKey(
+        "users.User", related_name="timetasks", on_delete=models.CASCADE
+    )
+    checked = models.BooleanField(default=False)
+    part = models.CharField(choices=PART_CHOICES, max_length=10, blank=True)
+    created_date = models.DateField(auto_now_add=True)
+
+
 class WeekTask(TimeStampedModel):
-    week_index = models.PositiveIntegerField()
+    week_index = models.PositiveSmallIntegerField(default=0)
+    user = models.ForeignKey(
+        "users.User", related_name="weektasks", on_delete=models.CASCADE, default=""
+    )
     contents = models.TextField(null=True, blank=True)
 
 
 class WeekPlan(TimeStampedModel):
     user = models.ForeignKey(
-        "users.User", related_name="weekplans", on_delete=models.CASCADE
+        "users.User", related_name="weekplans", on_delete=models.CASCADE,
     )
-    week_index = models.PositiveIntegerField()
+    week_index = models.PositiveIntegerField(default=0)
     contents = models.TextField(null=True, blank=True)
