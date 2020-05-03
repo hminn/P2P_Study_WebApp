d114efbdfcb650a7bb6ed75cfb7a03fb5553ffe6
hminn <hmkim0008@gmail.com>
2020-05-03 18:20:31 +0900

    Update forms.py
    - TimeTask에 대한 Form 추가

--- todos/forms.py
+++ todos/forms.py
@@ -1,10 +1,24 @@
 from django import forms
-from .models import TodoList
+from .models import TodoList, TimeTask
 
 
 class TodoForm(forms.ModelForm):
     class Meta:
         model = TodoList
+        fields = [
+            "contents",
+        ]
+        labels = {
+            "contents": "",
+        }
+        widgets = {
+            "contents": forms.TextInput(attrs={"placeholder": "Write to do"}),
+        }
+
+
+class TimeTaskForm(forms.ModelForm):
+    class Meta:
+        model = TimeTask
         fields = [
             "contents",
         ]
