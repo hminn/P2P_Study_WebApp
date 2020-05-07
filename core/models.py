c204d5d7f92c57cfe41ce50bd49d576b9fd8ff1d
hminn <hmkim0008@gmail.com>
2020-05-07 18:57:55 +0900

    Update models.py
    - 새로운 객체 생성 시에 Not Null Constraint Error가 발생하여 Null=True 옵션을 추가

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ class TimeStampedModel(models.Model):
 
     """ Time Stamped Model """
 
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
